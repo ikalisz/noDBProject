4223d5bebf119f375d528ec4a44740b9eabf3135
Ian Kaliszewski <ikalisz@yahoo.com>
2019-05-06 15:14:32 -0600

    created api skeletons and started work html skele

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const images = require()
 const port = 3255
 
 app.use(cors())
 app.use(express.json())
+
+
 
 
 app.listen(port, () => {
---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header>
+            <h3>
+                Home
+            </h3>
+            <div>
+                <h3>Posts</h3>
+                <h3>About</h3>
+            </div>
+        </header>
+    )
+}
