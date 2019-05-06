9b03887c9637b51d55e1852b0a8c8ebecc530cdc
Ian Kaliszewski <ikalisz@yahoo.com>
2019-05-06 14:35:30 -0600

    Set up layout of API

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 3255
+
+app.use(cors())
+app.use(express.json())
+
+
+app.listen(port, () => {
+    console.log('Working!')
+})
